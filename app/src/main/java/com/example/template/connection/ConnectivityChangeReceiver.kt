f28167801e140110b52d9709e1b0495e79b86c53
amirhosseinghafourian <amirhgh1377@gmail.com>
2020-11-29 09:10:40 +0330

    3-Check Connection Done

--- app/src/main/java/com/example/template/connection/ConnectivityChangeReceiver.kt
+++ app/src/main/java/com/example/template/connection/ConnectivityChangeReceiver.kt
@@ -3,22 +3,20 @@ package com.example.template.connection
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.net.ConnectivityManager
-import android.widget.Toast
 
 class ConnectivityChangeReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (ConnectivityManager.EXTRA_NETWORK == intent.action) {
-            val noConnectivity = intent.getBooleanExtra(
-                ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
-            )
-            if (noConnectivity) {
-                Toast.makeText(context, "disconnected", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(context, "connected", Toast.LENGTH_SHORT).show()
-            }
-        }
+//        if (ConnectivityManager.CONNECTIVITY_ACTION == intent.action) {
+//            val noConnectivity = intent.getBooleanExtra(
+//                ConnectivityManager.EXTRA_NO_CONNECTIVITY, false
+//            )
+//            if (noConnectivity) {
+//                Toast.makeText(context, "disconnected", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(context, "connected", Toast.LENGTH_SHORT).show()
+//            }
+//        }
     }
 
 }
--- app/src/main/java/com/example/template/network/ui/NetworkFragment.kt
+++ app/src/main/java/com/example/template/network/ui/NetworkFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.template.R
+import com.example.template.connection.ConnectionLiveData
 import com.example.template.connection.ConnectivityChangeReceiver
 
 class NetworkFragment : Fragment() {
@@ -20,7 +22,17 @@ class NetworkFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        val connectionLiveData = ConnectionLiveData(requireContext())
+        connectionLiveData.observe(viewLifecycleOwner, { isConnected ->
+            if (isConnected != null) {
+                if (isConnected) {
+                    Toast.makeText(requireContext(), "network connected", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(requireContext(), "network disconnected", Toast.LENGTH_SHORT).show()
+                }
+            }
 
+        })
     }
 
 }
