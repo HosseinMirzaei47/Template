f1d563d4df298af5c8477d9147bcbf3222b3d02c
amirhosseinghafourian <amirhgh1377@gmail.com>
2020-12-23 16:59:01 +0330

    11-ready for video update 2

--- app/src/main/java/com/example/template/home/domain/CityUseCase.kt
+++ app/src/main/java/com/example/template/home/domain/CityUseCase.kt
@@ -13,20 +13,20 @@ class CityUseCase @Inject constructor(
     override suspend fun execute(params: String): MutableList<String> {
         val list = mutableListOf<String>()
         delay(1000)
-        if ("خراسان رضوی".startsWith(params)) {
-            list.add("مشهد")
-            list.add("کاشمر")
-            list.add("نیشابور")
+        if ("khorasan razavi".startsWith(params)) {
+            list.add("mashhad")
+            list.add("kashmar")
+            list.add("neyshabur")
         }
-        if ("خراسان شمالی".startsWith(params)) {
-            list.add("بجنورد")
+        if ("khorasan shomali".startsWith(params)) {
+            list.add("bojnord")
         }
-        if ("خراسان جنوبی".startsWith(params)) {
-            list.add("بیرجند")
+        if ("khorasan jonubi".startsWith(params)) {
+            list.add("birjand")
         }
-        if ("تهران".startsWith(params)) {
-            list.add("تهران")
-            list.add("کرج")
+        if ("tehran".startsWith(params)) {
+            list.add("tehran")
+            list.add("karaj")
         }
         return list
     }
